2a02aa0810aeb33ccb73177fa23588f273eaba0c
Kevin <kevinlq75@gmail.com>
2022-01-21 23:52:27 +0100

    version propre post-Gillespie

--- Gillespie.py
+++ Gillespie.py
@@ -1,13 +1,17 @@
+from curses.ascii import SI
+from re import S
 from Interpreteurtxt import Param
+import random as rd
+import math as m
 
 ### Dans ce fichier, on rentrera le code qui sera impliqué pour la question 6
 
 
-
+# question a
 def lambd(S,
           I,
-          N = Param["population"],        
-          beta = Param["transmission"],
+          N     = Param["population"],        
+          beta  = Param["transmission"],
           gamma = Param["recuperation"],
         ):
 
@@ -24,4 +28,64 @@ def lambd(S,
   Variables renvoyées : 
   lambda.
   """
-  return beta/N*S*I+gamma*I
+  return beta/N*S*I+gamma*I #calcul de lambda à partir de la formule (8) 
+
+#question b
+def Delta_T(lamb):
+  """
+  Description : Calcule un incrément temporel aléatoirement tiré d'une distribution exponentielle.
+  ---
+  Variables d'entrée :
+  lamb : lambda.
+  Variables renvoyées :
+  L'incrément de temps.
+  """
+  return -m.log(rd.random())/lamb #calcul de DT à partir de la note 3 
+# partiellement question c 
+def pi(S,
+       I,
+       N     = Param["population"],
+       beta  = Param["transmission"],
+       gamma = Param["recuperation"]
+       ):
+  """
+  Description : Calcule la probabilité de l'evenement "pendant l'instant Delta t, il y a eu unne infection".
+  ---
+  Variables d'entrée
+  S     : Nombre d'individus sains.
+  I     : Nombre d'individus infectés.
+  N     : Population totale.
+  beta  : Facteur de transmission.
+  gamma : Facteur de recuperation.
+  ---
+  Variables renvoyées
+  la probabilité pi.
+  """
+  return beta*S*I/N/lambd(S,I,N,beta,gamma) #calcul de pi 
+
+
+#fin de la question c et questions d et e 
+def T_plus_Delta_T(t,
+                   S,
+                   I,
+                   N     = Param["population"],
+                   beta  = Param["transmission"],
+                   gamma = Param["recuperation"]
+                   ):
+  """
+  Description : fait évoluer la situation de l'épidémie à l'instant t à celui t+dt
+  ---
+  Variables d'entrée :
+  t     : instant où la fonction est appelée.
+  S     : Nombre d'individus sains.
+  I     : Nombre d'individus infectés.
+  N     : Population totale.
+  beta  : Facteur de transmission.
+  gamma : Facteur de recuperation.
+  ---
+  Variables renvoyées : 
+  tuple contenant respectivement le vecteur (S,I,R) à l'instant t+dt et t+dt
+  """
+  if rd.random()<pi(S,I,N,beta,gamma): #Disjonction de cas 
+    return((S-1,I+1,N-S-I),t+Delta_T(lambd(S,I,N,beta,gamma)))
+  return((S,I-1,N-S-I+1),t+Delta_T(lambd(S,I,N,beta,gamma)))
---
+++ etude.py
@@ -0,0 +1,125 @@
+from Interpreteurtxt import Param
+from deterministe import *
+from representation import *
+from Gillespie import *
+
+
+N     = Param["population"]      
+beta  = Param["transmission"]
+gamma = Param["recuperation"]
+Tf    = Param["temps de simulation"]
+N0    = Param["population saine initiale"]
+NP    = Param["nombre d'évaluations"]
+Ret   = Param["population rétablie initiale"]
+
+
+def etude_un_cas(save = False,
+                 nom  = "figure"
+                 ):
+
+    """
+    Description : Ensemble d'opérations permettant de résoudre le système d'équations déterministe à partir des fonctions du fichier deterministe.py .
+    ---
+    Variables d'entrée : 
+    save : Booléen, False par défaut. Détermine si le graphique généré par l'appel sera enregistré (True) ou non (False).
+    nom  : texte, "figure" par défaut. Détermine, le cas échéant, le nom du fichier sauvegardé. 
+    ---
+    Variables renvoyées :
+    une image affichée à l'écran ou enregistrée dans le même dossier d'execution de main.py.
+    """
+
+    (S,I,V,t) = deterministe_case()
+    ploteur([t,t,t],[S,I,V],["individus sains","individus infectés","individus rétablis"],[".",".","."],"temps","population",nom,save)
+
+
+def etude_R0(save = False,
+             nom  = "figure"):
+
+    """
+    Description : Ensemble d'opérations permettant d'étudier l'issue de l'épidémie par approche déterministe. La méthode utilisée ici est de fixer beta (respectivement gamma) et de faire varier gamme (respectivement beta) pour faire évoluer R0.
+    ---
+    Variables d'entrée : 
+    save : Booléen, False par défaut. Détermine si le graphique généré par l'appel sera enregistré (True) ou non (False).
+    nom  : texte, "figure" par défaut. Détermine, le cas échéant, le nom du fichier sauvegardé. 
+    ---
+    Variables renvoyées :
+    une image affichée à l'écran ou enregistrée dans le même dossier d'execution de main.py .
+    """
+    ##déclaration des variables 
+    taux_S_sur_N_gamma_cte = [] #creation de la liste des individus sains à l'issue de la simulation à gamma = cte et beta variable
+    taux_S_sur_N_beta_cte  = [] #creation de la liste des individus sains à l'issue de la simulation à beta = cte et gamma variable
+    R0=[i/50 for i in range(1,151)] #creation de l'intervalle du nombre R0
+
+    ##opérations 
+    for i in R0:
+        taux_S_sur_N_beta_cte.append(deterministe_case(beta=i*gamma,Renvoie_liste=False))  #simulation à beta = cte
+        taux_S_sur_N_gamma_cte.append(deterministe_case(gamma=beta/i,Renvoie_liste=False)) #simulation à gamma = cte
+
+    ##tracés 
+    ploteur([R0,R0],[taux_S_sur_N_beta_cte,taux_S_sur_N_gamma_cte],["à beta constant", "à gamma constant"],["x","."],"R0","Proportion de population",nom,save) #tracé
+
+
+def etude_GammaBeta_2D(save = False,
+                       nom = "figure"
+                       ):
+
+    """
+    Description : Ensemble d'opérations permettant d'étudier l'issue de l'épidémie par approche déterministe. La méthode utilisée ici est de faire varier beta ET gamma dans le but de tracer S(t=tfinal) = f(beta,gamma).
+    ---
+    Variables d'entrée : 
+    save : Booléen, False par défaut. Détermine si le graphique généré par l'appel sera enregistré (True) ou non (False).
+    nom  : texte, "figure" par défaut. Détermine, le cas échéant, le nom du fichier sauvegardé. 
+    ---
+    Variables renvoyées :
+    une image affichée à l'écran ou enregistrée dans le même dossier d'execution de main.py .
+    """
+    ##déclaration des variables 
+    gamma2d = [0.1*i for i in range(1,20)] #création de l'intervalle gamma
+    beta2d  = [0.1*i for i in range(1,20)] #création de l'intervalle beta
+    X,Y     = np.meshgrid(gamma2d, beta2d) #création du maillage, noms X et Y pour coincider avec les conventions numpy et matplotlib (Z=f(X,Y))
+    Z=[] #futur tableau contenant les issues, nom Z pour coincider avec les conventions numpy et matplotlib (Z=f(X,Y))
+    RZ=[]#futur tableau contenant les R0, nom RZ pour coincider avec les conventions numpy et matplotlib (RZ=f(X,Y))
+
+    ##opérations 
+    for i in beta2d: #parcours sur gamma puis sur beta
+        temp=[] #tableau qui à l'issue de la boucle contiendra une ligne correspondant à Z = f(X,Yi)
+        tempR=[] #tableau qui à l'issue de la boucle contiendra une ligne correspondant à RZ = f(X,Yi)
+        for j in gamma2d:
+            temp.append(deterministe_case(beta=i,gamma=j,Renvoie_liste=False)) #remplissage de temp
+            tempR.append(i/j) #remplissage de tempR
+        Z.append(temp) #injection de temp pour completer une ligne de Z à l'issue de la boucle imbriquée 
+        RZ.append(tempR) #injection de tempR pour completer une ligne de RZ à l'issue de la boucle imbriquée 
+
+    ##tracés 
+    ploteur2d(X,Y,Z,"taux de recuperation","taux de transmission",nom,save) #tracé
+    ploteur2d(X,Y,RZ,"taux de recuperation","taux de transmission",nom,save) #tracé
+
+
+def etude_Gillespie(save = False, nom = "figure"):
+
+    """
+    Description : Ensemble d'opérations permettant d'étudier l'issue de l'épidémie par approche des chaînes de Markov à temps continue.
+    ---
+    Variables d'entrée : 
+    save : Booléen, False par défaut. Détermine si le graphique généré par l'appel sera enregistré (True) ou non (False).
+    nom  : texte, "figure" par défaut. Détermine, le cas échéant, le nom du fichier sauvegardé. 
+    ---
+    Variables renvoyées :
+    une image affichée à l'écran ou enregistrée dans le même dossier d'execution de main.py .
+    """
+    ##déclaration des variables
+    T = [0] #liste des temps 
+    St = [N0] #liste des populations saines à l'instant t 
+    It = [N-N0-Ret] #liste des populations infectées à l'instant t 
+    Rt = [Ret] #liste des populations rétablies à l'instant t 
+
+    ##opérations 
+    while T[-1]<Tf and It[-1]!=0: #cas d'arret = le temps final est dépassé ou bien il n'y a plus d'infecté  
+        temp = T_plus_Delta_T(T[-1],St[-1],It[-1]) #si le cas d'arret n'est pas vérifié, on établit la situation à l'instant d'après 
+        T.append(temp[1])
+        St.append(temp[0][0])
+        It.append(temp[0][1])
+        Rt.append(temp[0][2])
+
+    ##tracés 
+    ploteur([T,T,T],[St,It,Rt],["individus sains","individus infectés","individus rétablis"],["-","-","-"],"temps","population",nom,save)
